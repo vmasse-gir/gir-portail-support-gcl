78a494cae484d86aea3745aa99659017fe4fb4d2
Victor Massé <v.masse@gir.fr>
2021-07-05 11:14:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const cacheName = 'gir-portail-support-gcl-v1';
 
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key =>  key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
+});
